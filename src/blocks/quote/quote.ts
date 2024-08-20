42867c507cd1ae0c0438404b64e8ca47e5f5e95a
samuelecalamita <samuelecalamita@gmail.com>
2024-08-20 14:21:44 +0200

    improve move instrumentation helper

--- src/blocks/quote/quote.ts
+++ src/blocks/quote/quote.ts
@@ -3,6 +3,21 @@ import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
 import './quote.scss';
 import { cleanUpBlock } from 'Utils/cleanUpBlock';
+import { moveInstrumentation } from 'Helpers/moveInstrumentation';
+
+interface MoveInstrumentationsOption {
+  from: Element;
+  to: Element;
+}
+
+type MoveInstrumentationsOptions = MoveInstrumentationsOption[] | MoveInstrumentationsOption;
+
+const moveInstrumentations = (options: MoveInstrumentationsOptions) => {
+  const opts = Array.isArray(options) ? options : [options];
+  opts.forEach(({ from, to }) => {
+    moveInstrumentation(from, to);
+  });
+};
 
 const template = ({ quote, author }: { quote: string; author: string }) => {
   return html`
@@ -17,4 +32,12 @@ export default function decorate(block: HTMLElement) {
 
   cleanUpBlock(block);
   render(template({ quote: quote.innerHTML, author: author.innerText }), block);
+
+  const quoteContent = block.querySelector('.quote__content') as HTMLQuoteElement;
+  const quoteAuthor = block.querySelector('.quote__author') as HTMLParagraphElement;
+
+  moveInstrumentations([
+    { from: quote, to: quoteContent },
+    { from: author, to: quoteAuthor },
+  ]);
 }
--- src/helpers/moveInstrumentation.ts
+++ src/helpers/moveInstrumentation.ts
@@ -1,16 +1,66 @@
+import { DebuggerService } from '@kluntje/services';
+
 import { moveAttributes } from './moveAttributes';
 
 /**
- * Move instrumentation attributes from a given element to another given element.
- * @param {Element} from the element to copy attributes from
- * @param {Element} to the element to copy attributes to
+ * Move instrumentation attributes that starts with "data-aue-" or "data-richtext-"  from a given element to another given element.
+ * @param {Element | string} from the element or CSS selctor to copy attributes from
+ * @param {Element | string} to the element or CSS selector to copy attributes to
+ * @returns {void}
+ * @example
+ * // With CSS selectors as arguments:
+ * moveInstrumentation('.quote', '.quote__content');
+ *
+ * // Or with elements as arguments:
+ * moveInstrumentation(document.querySelector('.quote'), document.querySelector('.quote__content'));
+ *
+ * // Or with a mix of CSS selector and element as arguments:
+ * moveInstrumentation('.quote', document.querySelector('.quote__content'));
+ *
+ * // Example HTML:
+ * // <blockquote class="quote" data-aue-attribute="value">Quote</blockquote>
+ * // <div class="quote__content"></div>
+ *
+ * // Usage:
+ * moveInstrumentation('.quote', '.quote__content');
+ *
+ * // Result:
+ * // <blockquote class="quote">Quote</blockquote>
+ * // <div class="quote__content" data-aue-attribute="value"></div>
  */
-export function moveInstrumentation(from: Element, to: Element) {
-  moveAttributes(
-    from,
-    to,
-    [...from.attributes]
-      .map(({ nodeName }) => nodeName)
-      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-'))
-  );
+export function moveInstrumentation(from: Element | string, to: Element | string): void {
+  try {
+    let fromEl: Element | null;
+    let toEl: Element | null;
+
+    if (typeof from === 'string') {
+      fromEl = document.querySelector(from);
+
+      if (fromEl === null) {
+        throw new Error(`Element with selector ${from} not found`);
+      }
+    } else {
+      fromEl = from;
+    }
+
+    if (typeof to === 'string') {
+      toEl = document.querySelector(to);
+
+      if (toEl === null) {
+        throw new Error(`Element with selector ${to} not found`);
+      }
+    } else {
+      toEl = to;
+    }
+
+    moveAttributes(
+      fromEl,
+      toEl,
+      [...fromEl.attributes]
+        .map(({ nodeName }) => nodeName)
+        .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-'))
+    );
+  } catch (error) {
+    DebuggerService.error(error);
+  }
 }
