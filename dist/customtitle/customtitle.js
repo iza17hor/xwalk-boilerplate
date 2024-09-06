50718d628c80eec39be96eac0712445dd28878db
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-09-06 15:21:05 +0200

    reset

--- dist/customtitle/customtitle.js
+++ dist/customtitle/customtitle.js
@@ -1,2 +1,2 @@
-import{x as n}from"../__chunks__/lit-element.BYUc91cE.js";import{r}from"../__chunks__/renderBlock.D2kyhEqO.js";import"../__chunks__/index.module.DQI0LCqS.js";import"../__chunks__/cleanUpBlock.DyBWs1cd.js";import"../__chunks__/moveInstrumentation.CxEVSfMM.js";const l=t=>n`<h1>${t}</h1>`;function a(t){const e=t.children[0].children[0];console.log(">>> textelement",e,"attrs",e.attributes);const o=e.textContent||"";console.log(">>> textcontent",o),console.log(">>> customTitle block",t),r({template:l(o),container:t,moveInstrumentationsOptions:{from:e.querySelector("p"),to:t.querySelector("h1")}})}export{a as default};
+function t(o){console.log(">>>",o)}export{t as default};
 //# sourceMappingURL=customtitle.js.map
--- src/blocks/customtitle/customtitle.ts
+++ src/blocks/customtitle/customtitle.ts
@@ -1,28 +1,31 @@
-import { html, TemplateResult } from 'lit';
+// import { html, TemplateResult } from 'lit';
 
 import './customtitle.scss';
-import { renderBlock } from 'Helpers/renderBlock';
+// import { renderBlock } from 'Helpers/renderBlock';
 
-const template = (text: string): TemplateResult => {
-  return html`<h1>${text}</h1>`;
-};
+// const template = (text: string): TemplateResult => {
+//   return html`<h1>${text}</h1>`;
+// };
 
 export default function (block: HTMLElement) {
-  const textElement = block.children[0].children[0];
-  // eslint-disable-next-line no-console
-  console.log('>>> textelement', textElement, 'attrs', textElement.attributes);
-  const text = textElement.textContent || '';
-  // eslint-disable-next-line no-console
-  console.log('>>> textcontent', text);
-  // eslint-disable-next-line no-console
-  console.log('>>> customTitle block', block);
+  console.log('>>>', block);
+  // const textElement = block.children[0].children[0];
 
-  renderBlock({
-    template: template(text),
-    container: block,
-    moveInstrumentationsOptions: {
-      from: textElement.querySelector('p') as Element,
-      to: block.querySelector('h1') as Element,
-    },
-  });
+  // renderBlock({
+  //   template: template(text),
+  //   container: block,
+  //   moveInstrumentationsOptions: {
+  //     from: textElement,
+  //     to: block.querySelector('h1') as Element,
+  //   },
+  // });
+
+  // console.log('>>> render finished', block);
+  // Array.from(attributes).forEach((attr) => {
+  //   const value = block.querySelector('p')!.getAttribute(attr);
+  //   if (value) {
+  //     to.setAttribute(attr, value);
+  //     from.removeAttribute(attr);
+  //   }
+  // });
 }
