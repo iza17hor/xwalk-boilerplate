e6eaa04b3d7ad315f18b96be5859a99a7ba86d1f
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-09-06 21:18:20 +0200

    wip

--- dist/customtitle/customtitle.js
+++ dist/customtitle/customtitle.js
@@ -1,11 +1,2 @@
-import{j as d,T as p,x as f}from"../__chunks__/lit-element.BYUc91cE.js";import{c as y}from"../__chunks__/cleanUpBlock.DyBWs1cd.js";/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const h=Symbol.for(""),T=(e,...r)=>({_$litStatic$:r.reduce((t,a,i)=>t+(n=>{if(n._$litStatic$!==void 0)return n._$litStatic$;throw Error(`Value passed to 'literal' function must be a 'literal' result: ${n}. Use 'unsafeStatic' to pass non-literal values, but
-            take care to ensure page security.`)})(a)+e[i+1],e[0]),r:h}),l=["h1","h2","h3","h4","h5","h6"],$=[...l],m=l.reduce((e,r)=>(e[r]=T`${r}`,e),{});function v(e){return[...e.attributes].map(({nodeName:t})=>t).filter(t=>t.startsWith("data-aue-")||t.startsWith("data-richtext-")).map(t=>({[t]:e.getAttribute(t)}))}function b(e){return r=>{const t=e.querySelector(`[data-aue-prop="${r}"]`);return{element:t,textContent:(t==null?void 0:t.textContent)||void 0,innerHTML:(t==null?void 0:t.innerHTML)||void 0,dataAttributes:t?v(t):void 0}}}const S=({titleText:e,titleType:r="h2"})=>{if(!e)return p;const t=m[r];return f`
-    <div style="background: red">
-      <${t} data-js-title-type>${e}</${t}>
-    </div>
-  `};function A(e){const r=b(e),{textContent:t,dataAttributes:a}=r("customTitle"),{textContent:i}=r("titleType"),n=$.includes(i)?i:"h2";y(e),d(S({titleText:t,titleType:n}),e,{});const o=e.querySelector("[data-js-title-type]");a==null||a.forEach(s=>{const u=Object.keys(s)[0],c=s[u];c&&(o==null||o.setAttribute(u,c))})}export{A as default};
+import{j as o,x as e}from"../__chunks__/lit-element.BYUc91cE.js";import{c as r}from"../__chunks__/cleanUpBlock.DyBWs1cd.js";const l=()=>e`<div style="background:#ff0"><h2>Hello World</h2></div>`;function f(t){r(t),o(l(),t)}export{f as default};
 //# sourceMappingURL=customtitle.js.map
--- src/blocks/customtitle/customtitle.ts
+++ src/blocks/customtitle/customtitle.ts
@@ -1,49 +1,52 @@
-import { html, nothing, render, TemplateResult } from 'lit';
+import { html, render, TemplateResult } from 'lit';
 
 import { cleanUpBlock } from 'Utils/cleanUpBlock';
-import { headlineTagMap, HeadlineTags, validHeadlineTags } from 'Utils/tagMap';
-import { getElementData } from 'Utils/getElementData';
 
 import './customtitle.scss';
 
-type TemplateProps = {
-  titleText?: string;
-  titleType?: HeadlineTags | undefined;
-  titleAttributes?: Record<string, string>;
-};
+// type TemplateProps = {
+//   titleText?: string;
+//   titleType?: HeadlineTags | undefined;
+//   titleAttributes?: Record<string, string>;
+// };
 
-const template = ({ titleText, titleType = 'h2' }: TemplateProps): TemplateResult | typeof nothing => {
-  if (!titleText) {
-    return nothing;
-  }
+// const template = ({ titleText, titleType = 'h2' }: TemplateProps): TemplateResult | typeof nothing => {
+//   if (!titleText) {
+//     return nothing;
+//   }
 
-  const tag = headlineTagMap[titleType];
+//   const tag = headlineTagMap[titleType];
 
-  return html`
-    <div style="background: red">
-      <${tag} data-js-title-type>${titleText}</${tag}>
-    </div>
-  `;
+//   return html`
+//     <div style="background: red">
+//       <${tag} data-js-title-type>${titleText}</${tag}>
+//     </div>
+//   `;
+// };
+
+const template2 = (): TemplateResult => {
+  return html`<div style="background: yellow"><h2>Hello World</h2></div> `;
 };
 
 export default function (block: HTMLElement) {
-  const getDataForProperty = getElementData(block);
-  const { textContent: titleText, dataAttributes: titleAttributes } = getDataForProperty('customTitle');
-  const { textContent: titleType } = getDataForProperty('titleType');
+  // const getDataForProperty = getElementData(block);
+  // const { textContent: titleText, dataAttributes: titleAttributes } = getDataForProperty('customTitle');
+  // const { textContent: titleType } = getDataForProperty('titleType');
 
-  // Ensure that titleType is one of the valid HeadlineTags or use a default ('h2')
-  const normalizedTitleType = validHeadlineTags.includes(titleType as HeadlineTags)
-    ? (titleType as HeadlineTags)
-    : 'h2';
+  // // Ensure that titleType is one of the valid HeadlineTags or use a default ('h2')
+  // const normalizedTitleType = validHeadlineTags.includes(titleType as HeadlineTags)
+  //   ? (titleType as HeadlineTags)
+  //   : 'h2';
 
   cleanUpBlock(block);
-  render(template({ titleText, titleType: normalizedTitleType }), block, {});
+  // render(template({ titleText, titleType: normalizedTitleType }), block, {});
+  render(template2(), block);
 
-  const headline = block.querySelector('[data-js-title-type]');
-  titleAttributes?.forEach((attr) => {
-    const key = Object.keys(attr)[0];
-    const value = attr[key];
-    if (!value) return;
-    headline?.setAttribute(key, value);
-  });
+  // const headline = block.querySelector('[data-js-title-type]');
+  // titleAttributes?.forEach((attr) => {
+  //   const key = Object.keys(attr)[0];
+  //   const value = attr[key];
+  //   if (!value) return;
+  //   headline?.setAttribute(key, value);
+  // });
 }
